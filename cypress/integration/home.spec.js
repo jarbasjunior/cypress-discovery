57e14096abbacaea1acf183727d0f2b811d0dac6
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-13 15:20:56 -0300

    adjust: url cypress patterns

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe('Ao acessar home page:', () => {
   it('Aplicação deve estar online', () => {
-    cy.visit('https://buger-eats.vercel.app');
+    cy.visit('/');
     cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
   });
 });
